6ed859084c33b1cdb51a863338f46f75bf43f2bc
Vibha199 <112160132+Vibha199@users.noreply.github.com>
2024-07-02 17:43:48 +0530

    Add files via upload

---
+++ Cucumber/Activities/testRunner.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class testRunner {
+    package testRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            features = "src/test/java/Features"
+            , glue = {"stepDefinitions"}
+            , tags = "@activity4"
+            , plugin = "html:src/reports/HTML_Report.html"
+    )
+
+    public class testRunner {
+
+    }
+
+
+}
